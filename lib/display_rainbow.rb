d4b06d3ad1a845b36e08214fb1de9b3f49c41276
Juan Ruiz <juankboards@hotmail.com>
2015-12-03 01:24:12 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
-# Write your #display_rainbow method here
+def display_rainbow (colors = ["red"])
+  formatted_colors = []
+  colors.length.times {|i| formatted_colors << "#{colors[i][0].upcase}: #{colors[i]}"}
+  puts formatted_colors.join(", ")
+end
