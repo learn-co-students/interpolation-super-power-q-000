36a96cf224c2ab0c56a064ef4eb1a9089ab4a1a9
Abhi Pillai <abhi.pillai1@gmail.com>
2015-12-10 23:48:44 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  out = []
+  colors[0]
+  colors[1]
+  colors[2]
+  colors[3]
+  colors[4]
+  colors[5]
+  colors[6]
+  colors.each do |c|
+    out << "#{c[0].capitalize}: #{c}"
+  end
+  puts out.join(", ")
+end
