88e9fe5ee511950cbb13f546f6b91a7d7db9c47b
Chen Yang <yangc5@mail.uc.edu>
2015-12-20 18:46:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  output=""
+  idx = 0
+  while idx < colors.length
+    output += colors[idx][0].upcase
+    output +=": "
+    output += colors[idx]
+    output += ", "
+    idx += 1
+  end
+
+  output.chomp!(", ")
+  puts output
+end
+
