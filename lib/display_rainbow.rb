fdb89a31035906e45ac392bcd5926302359a9eb9
Ashley <serenehopes@gmail.com>
2015-12-28 08:19:20 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(hash)
+  puts "R: #{hash[0]}, O: #{hash[1]}, Y: #{hash[2]}, G: #{hash[3]}, B: #{hash[4]}, I: #{hash[5]}, V: #{hash[6]}"
+end
