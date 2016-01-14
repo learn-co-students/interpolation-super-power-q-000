cf9c3acb5b37b9e7ffb4d91a4329f02a686f7f89
wolfsandle <wolfsandle@gmail.com>
2016-01-14 15:07:05 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+#  1. Define a method, `#display_rainbow`, in `lib/display_rainbow.rb`
+#  2. `#display_rainbow` should print out the colors of the rainbow in the
+#     following format: `"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+#  3. It should accept an array containing the colors as an argument`
+#  `colors` will be passed in as: `['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']`
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+
+
+
+
