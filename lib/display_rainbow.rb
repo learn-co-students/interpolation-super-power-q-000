d0dc22fb802f91c8b7741693ad68e5ddc2f9dd91
rustyflakes <juanblizarraga@gmail.com>
2016-01-20 12:25:43 -0500

    interpolation super power

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(value)
+  puts "R: #{value[0]}, O: #{value[1]}, Y: #{value[2]}, G: #{value[3]}, B: #{value[4]}, I: #{value[5]}, V: #{value[6]}"
+end
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+display_rainbow(colors)
