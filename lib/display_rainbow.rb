983c71e2899496d2337e436e23f955594b1220b9
Aina Pinto <aina@pintowebdevelopment.com>
2016-02-13 09:10:59 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  # puts colors.inject([]) { |memo, color| memo << "#{color[0].upcase}: #{color}" }.join(", ")
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
