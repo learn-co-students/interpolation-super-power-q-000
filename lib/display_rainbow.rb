2f3179c1a0f510499848dc17f5d10f893f7a3477
kwebster2 <kevin.r.webster@gmail.com>
2016-02-14 15:59:47 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+def display_rainbow (feature)
+  puts "R: #{feature[0]}, O: #{feature[1]}, Y: #{feature[2]}, G: #{feature[3]}, B: #{feature[4]}, I: #{feature[5]}, V: #{feature[6]}"
+end
