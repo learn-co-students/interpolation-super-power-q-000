f87d909ca0748b54560dbc3ecae854ff8525d838
nuke-dukem <karlneumann61@gmail.com>
2016-02-14 19:22:22 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+    print "#{colors[0].slice(0).upcase}: #{colors[0]}, "
+    print "#{colors[1].slice(0).upcase}: #{colors[1]}, "
+    print "#{colors[2].slice(0).upcase}: #{colors[2]}, "
+    print "#{colors[3].slice(0).upcase}: #{colors[3]}, "
+    print "#{colors[4].slice(0).upcase}: #{colors[4]}, "
+    print "#{colors[5].slice(0).upcase}: #{colors[5]}, "
+    print "#{colors[6].slice(0).upcase}: #{colors[6]}\n"
+end
