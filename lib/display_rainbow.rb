677b8d6223ec12ed6235c0f4fbcb2098096d5f1f
tingtingwei <ttw704@gmail.com>
2016-02-20 15:41:18 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
-# Write your #display_rainbow method here
+# Write your #display_rainbow method here. Display_colors will go through an array and print the first letter of the color capitalized, followed by a colon, followed by a space and the lowercase color, with commas separating each item in the array of colors.
+def display_rainbow(array)
+  i = 0
+  while i < array.length
+    print "#{array[i][0].capitalize}: #{array[i]}"
+    i += 1
+    if i < array.length
+      print ", "
+    end
+  end
+ print "\n"
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
