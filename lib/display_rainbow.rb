45afc70fd910b57a3accd99ceead36edf17ce57c
blessings008 <qualityworks360@gmail.com>
2016-02-22 04:45:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,5 @@
-require_relative "../lib/display_rainbow.rb"
-def #display_rainbow(colors)
-  colors = 'R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet'
- puts "The first color is #{red[0]}"
-puts "The second color is #{orange[1]}"
-puts "The third color is #{yellow[2]}"
-puts "The fourth color is #{green[3]}"
-puts "The fifth color is #{blue[4]}"
-puts "The sixth color is #{indigo[5]}"
-puts "The seventh color is #{violet[6]}"
+
+def display_rainbow(color)
+  color = [red, orange, yellow, green, blue, indigo, violet]
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]} I: #{color[5]}, V: #{color[6]}"
 end
-print #display_rainbow(colors)
