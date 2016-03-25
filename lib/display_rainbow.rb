72058b94ebb2f13abe84a98fd4eb7874b41b8a23
Lenn Hypolite <lenntriniman@hotmail.com>
2016-03-25 02:03:00 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,8 +3,8 @@
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow(display_colors)
- puts "R:#{display_colors[0]}, O:#{display_colors[1]}, Y:#{display_colors[2]}, G:#{display_colors[3]},
-B:#{display_colors[4]}, I:#{display_colors[5]}, V:#{display_colors[6]}"
+ puts "R: #{display_colors[0]}, O: #{display_colors[1]}, Y: #{display_colors[2]}, G: #{display_colors[3]},
+B: #{display_colors[4]}, I: #{display_colors[5]}, V: #{display_colors[6]}"
 end
 
 display_rainbow(colors)
