8bb532b8718a1913c23e36f3a6903c993c863fa3
alicaz <alicadirie@gmail.com>
2016-04-13 19:12:06 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,24 +1,4 @@
-# Write your #display_rainbow method here
-name = "bob"
-puts "Hello, #{name}"
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
-puts "1 plus 1 is #{2}!"
-
-three = 2+1
-name = "Bob"
-puts "1 plus 1 is #{1+1} and 1 plus 2 is #{three} and 2*2 is #{2*2}. oh and hey #{name}!"
-
-
-
-bob = ["Bob, 46, Juggular"]
-stefeni = ["Stafeni, 49, Firefighter"]
-
-puts  "Name: #{bob[0]}, #{bob[1]}, #{bob[2]}"
-puts  "Name: #{stefeni[0]},#{stefeni[1]}, #{stefeni[2]}"
-
-def print_business_card(bob)
-  puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}, "
-  puts "contact: #{bob[3]}"
-  bob = ["Bob" "46" "Juggler" "555-555"]
 end
-
