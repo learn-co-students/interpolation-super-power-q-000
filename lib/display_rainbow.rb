f76d5bb59f90e60ee95f34ed8142fa096f095a29
David Holton <it.arcanum@gmail.com>
2016-05-04 22:56:29 -0400

    built method to display colors

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+def display_rainbow(clrs)
+	puts "R: #{clrs[0]}, O: #{clrs[1]}, Y: #{clrs[2]}, G: #{clrs[3]}, B: #{clrs[4]}, I: #{clrs[5]}, V: #{clrs[6]}"
+end
+
+display_rainbow(colors)
