9839eb779f3c8973c9a3976a52940a8dd719d1b9
Brendan Mulligan <brendanm05@gmail.com>
2016-06-04 20:52:37 -0700

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+
+
+
+
+def display_rainbow(array)
+  color_array = ["R", "O", "Y", "G", "B", "I", "V"]
+  color_string = []
+  color_array.each_with_index do |color, index|
+    color_string << "#{color}: #{array[index]}"
+  end
+  puts color_string.join(", ")
+   # puts "R: #{array[0]}, O: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
+end
