a4255731ca9ba1084dc93fe86ca077749e4de862
Daniel Rosen <technite@gmail.com>
2016-06-11 17:57:51 +0000

    Update

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  colorLetters = ["R", "O", "Y", "G", "B", "I", "V"]
+  tempString = ""
+  indx = 0
+  while indx < colors.size
+    tempString += "#{colorLetters[indx]}: #{colors[indx]}"
+    if indx < colors.size-1
+      tempString += ", "
+    end
+    indx += 1
+  end
+  puts tempString
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
