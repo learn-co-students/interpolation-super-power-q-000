4b1bc25353ba5a340a6583a976d3ea149f33d676
Satu Barnhill <satu.barnhill@gmail.com>
2016-06-22 18:58:26 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  #do stuff with input colors
+  index = ['R','O','Y','G','B','I','V']
+  puts "#{index[0]}: #{colors[0]}, #{index[1]}: #{colors[1]}, #{index[2]}: #{colors[2]}, #{index[3]}: #{colors[3]}, #{index[4]}: #{colors[4]}, #{index[5]}: #{colors[5]}, #{index[6]}: #{colors[6]}"
+end
