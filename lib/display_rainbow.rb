a03f1e61b5fd4a5dc8481361cf1410881e045349
Alicia Stillwelll <alicia.stillwell@gmail.com>
2016-07-20 17:01:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,12 @@
 def display_rainbow (colors)
+  #Initial answer below, but I wanted to try with a loop to account for variable colors
   #puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
   str = ""
-  colors.each {|c| str.concat(c[0].upcase + ": " + c + ", ")}
+
+  for i in 0..colors.length-1
+    str.concat(colors[i][0].upcase + ": " + colors[i] + ", ")
+  end
+
   puts str[0,str.length-2]
 end
