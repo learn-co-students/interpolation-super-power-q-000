f41bdff08c9214b0a8ddfc7377318bbc5ff1894a
Kyahan Flynch <kyahanflynch@gmail.com>
2016-07-26 08:16:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow(order)
+  puts """R: #{order[0]}, O: #{order[1]}, Y: #{order[2]}, G: #{order[3]}, B: #{order[4]}, I: #{order[5]}, V: #{order[6]}"""
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+#display_rainbow(color)
+#Why is the above syntax not correct for the lesson and the one used for invocation better?
