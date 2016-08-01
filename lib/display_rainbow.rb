f9023118a93e54c59fbff671c751bb87c270bcd7
mo6709 <mo6709@gmail.com>
2016-08-01 15:58:51 -0400

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(data)
+	puts "R: #{data[0]}, O: #{data[1]}, Y: #{data[2]}, G: #{data[3]}, B: #{data[4]}, I: #{data[5]}, V: #{data[6]}"
+end
