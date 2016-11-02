9c9ede8de381774441b096d2f5e3b760fd3536ee
Chris Hague <christopher.a.hague@gmail.com>
2016-11-02 20:32:49 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+def display_rainbow(arr)
+  ids = ["R", "O", "Y", "G", "B", "I", "V"]
+  puts "#{ids[0]}: #{arr[0]}, #{ids[1]}: #{arr[1]}, #{ids[2]}: #{arr[2]}, #{ids[3]}: #{arr[3]}, #{ids[4]}: #{arr[4]}, #{ids[5]}: #{arr[5]}, #{ids[6]}: #{arr[6]}"
+end
